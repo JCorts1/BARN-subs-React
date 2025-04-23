3843ae538bad18e13f9a5c1ea453c6a772affe50
JCorts1 <cortesjay1991@gmail.com>
2025-04-23 12:23:24 +0200

    right side has been created.

---
+++ src/components/LeftContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../components/LeftContainer.css"
+
+const LeftContainer = () => {
+  return (
+    <div className="left-container">
+        <div>
+            <h1>COFFEE SUBSCRIPTIONS</h1>
+            <p>Explore the best Single Origin flavour out there with our Seasonal Coffee Subscriptions. 
+            Driven by quality and sustainability, our coffee tastes better for a reason.</p>
+        </div>
+    </div>
+  );
+};
+
+export default LeftContainer;
